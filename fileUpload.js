b31d46a7582a4e666a8aebb5d24980dc302e18a3
ning <maxspeed40k@gmail.com>
2012-06-07 16:57:33 +0800

    v 0.1

--- fileUpload.js
+++ fileUpload.js
@@ -2,41 +2,96 @@
 (function($){
     $.widget('ui.fileUpload', {
         options: {
-            // 命名空间
-            namespace: undefined,
             // 最大文件切片大小
             maxChunkSize: 1024 * 1024 * 1,
-            progress: null
+            // 自动开始上传
+            autoStart: true
         },
+        _files:[],
+        _status:[],
+        _que:[],
         _create: function() {
             var that = this;
-            that._files=that._que=[];
             this.element.bind('change.fileUpload', function(){
-                var files = this.files;
-                // 取得每个文件当前的服务器路径
-                for(i=0; i<files.length; i++) {
-                    var file = files[i];
-                    that._files.push({
-                        lastModifiedDate: Math.ceil(file.lastModifiedDate.getTime()/1000),
+                that._files = this.files;
+                for(var i=0; i<that._files.length; i++) {
+                    var file = that._files[i];
+                    that._status[i]={
+                        lastModified: Math.ceil(file.lastModifiedDate.getTime()/1000),
                         name: file.name,
-                        size: file.size,
-                        loaded:0
-                    });
+                        size: file.size
+                    };
                 }
-                $.ajax({
-                    url:"fileUpload.php",
-                    data: {
-                        action: 'init',
-                        files: that._files
-                    },
-                    type:'POST',
-                    dataType: 'json',
-                    success:function(json) {
-                        
-                    }
-                });
-                // 
+                if(that.options.autoStart) {
+                    that.start();
+                }
             });
+        },
+        start:function() {
+            this._start();
+        },
+        _start : function() {
+            var that = this;
+            $.ajax({
+                url:"fileUpload.php?action=init",
+                data: {
+                    files: that._status
+                },
+                type:'POST',
+                dataType: 'json',
+                success:function(json) {
+                    for(var i=0;i<json.length;i++) {
+                        that._status[i].result = json[i] ? 0 : 2; // 0,队列中，1，上传中，2，重复，2，成功，3，失败，4，已删除
+                        if(that._status[i].result===0) {
+                            that._upload(i);
+                        }
+                    }
+                    
+                }
+            });
+        },
+        _upload:function(i) {
+            var file = this._files[i],
+            part = 0,
+            start = 0,
+            trunk_size = 1024*1024*10,
+            end = 0,
+            length = trunk_size;
+            while( start < file.size ) {
+                part++;
+                end = start + trunk_size;
+                if(end > file.size) {
+                    end = file.size;
+                }
+                length = end - start;
+                var xhr =  new XMLHttpRequest();
+                xhr.upload.addEventListener("progress", function(e){
+                    console.log("progress:"+e.loaded/e.total);
+                }, false);
+                xhr.addEventListener("load", function(e){
+                    var resp = e.target.responseText;
+                    try {
+                        var json = JSON.parse(resp);
+                        if(json.error) {
+                            throw json.msg;
+                        }
+                    } catch (err) {
+                        if(typeof(err)=='string') {
+                            console.log(err);
+                        } else {
+                            console.log(err.message);
+                        }
+                    }
+                }, false);
+                xhr.upload.addEventListener("error", function(e){
+                    }, false);
+                xhr.addEventListener("abort", function(e){
+                    }, false);
+                xhr.open("POST", "fileUpload.php?action=upload&name="+ file.name + "&length="+ length 
+                    + "&start="+ start + "&size="+ file.size + "&lastModified="+ Math.ceil(file.lastModifiedDate.getTime()/1000));
+                xhr.send(file.webkitSlice(start, end));
+                start = end;
+            }
         }
         
     });
--- fileUpload.php
+++ fileUpload.php
@@ -4,23 +4,41 @@ set_time_limit(0);
 
 $upload_dir = "upload/";
 
-$action = $_POST['action'];
+$action = $_GET['action'];
 if ($action == 'init') {
-    // 初始化一个上传，并返回唯一文件名称
+    // 初始化上传
     $files = $_POST['files'];
-    foreach ($files as &$v) {
-        $pathinfo = pathinfo($v['name']);
-        $ext = $pathinfo['extension'];
-        $v['location'] = $upload_dir . uniqid() . "." . $ext;
+    $return = array();
+    foreach ($files as $v) {
+        if (file_exists($upload_dir . $v['name'])) {
+            if (filesize($upload_dir . $v['name']) == $v['size']) {
+                $return[] = false;
+            } else {
+                @unlink($upload_dir . $v['name']);
+                $return[] = true;
+            }
+        } else {
+            $return[] = true;
+        }
     }
-    exit(json_encode($files));
-} elseif ($action == 'range') {
-    // 取得文件已上传部分
-
-    print_r($_POST);
+    exit(json_encode($return));
 } elseif ($action == 'upload') {
-    // 上传文件
-
+    // 上传文件片段
+    try {
+        $src = fopen('php://input', 'rb');
+        if ($dest = fopen($upload_dir . $_GET['name'], 'cb')) {
+            fseek($dest, $_GET['start']);
+            stream_copy_to_stream($src, $dest, $_GET['length']);
+            fclose($dest);
+        }
+        exit(json_encode(array('error' => 0)));
+    } catch (Exception $e) {
+        header("Status:500 Internal Server Error");
+        exit;
+    }
+} elseif ($action == 'delete') {
+    // 删除上传文件
+    @unlink($upload_dir . $_GET['name']);
     print_r($_POST);
 }
 
